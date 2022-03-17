2c9902613f4b3e86aa31583132d4e0cbc5cb3745
paQQuete <boris.vladimirov13@gmail.com>
2022-03-17 16:52:59 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import json
+import os
+
+
+class DataSoup():
+    def __init__(self, directory):
+        self._directory = self._parseFiles(directory)
+        pass
+
+    def _parseFiles(self, dir: str) -> list:
+        pass
+
+    def _parseDataFromFile(self, filename: str) -> dict:
+        pass
+
+    def makeCollection(self)
+        pass:
+
+
+
+
+
+
+if __name__ == '__main__':
+
+
