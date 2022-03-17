bf49672b5d54c3aac250c8d02d356ae941065488
paQQuete <boris.vladimirov13@gmail.com>
2022-03-17 17:39:08 +0300

    ...

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 import json
 import os
+import codecs
+
+DIR = os.chdir(r"\\10.11.178.200\сеть\владимиров\responce")
 
 
 class DataSoup():
     def __init__(self, directory):
-        self._directory = self._parseFiles(directory)
+        self._filesList = self._parseFiles(directory)
+        self._parseDataFromFile()
         pass
 
     def _parseFiles(self, dir: str) -> list:
-        pass
+        return os.listdir(dir)
 
-    def _parseDataFromFile(self, filename: str) -> dict:
-        pass
 
-    def makeCollection(self)
-        pass:
+    def _parseDataFromFile(self) -> dict:
+        flist = self._filesList
+        for each in flist:
+            with codecs.open(each, mode='r', encoding='utf-8-sig') as f:
+                file = f.read()
+                file = file.replace('\r', '').replace('\n', '')
+                data = json.loads(file)
+                pass
+
+    def makeCollection(self):
+        pass
 
 
 
@@ -22,5 +33,5 @@ class DataSoup():
 
 
 if __name__ == '__main__':
-
+    A = DataSoup(DIR)
 
