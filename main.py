441c45373d79c9d5491ce9b40b78f9062188d54b
Boris <boris.vladimirov13@gmail.com>
2022-03-20 02:32:06 +0300

    change classes

--- main.py
+++ main.py
@@ -1,50 +1,57 @@
 import json
 import os
 import codecs
-
+from datetime import datetime
 import sensdata
 
 '''
 all sensitive data in sensdata.py
 '''
 
-
 DIR = sensdata.DIR
 
 
+class GetDataFiles():
 
-
-class DataSoup():
-    def __init__(self, directory):
-        self._filesList = self._parseFiles(directory)
-        self._parseDataFromFile()
-        pass
-
-    def _parseFiles(self, dir: str) -> list:
+    @staticmethod
+    def getFromDirectory(dir) -> list:
+        os.chdir(dir)
         return os.listdir(dir)
 
 
-    def _parseDataFromFile(self) -> dict:
-        outdict = dict()
-        flist = self._filesList
+class DataSoup():
+
+    @staticmethod
+    def parseDataFromFile(flist) -> list[dict]:
+        outlist = list()
+
         for each in flist:
+            outdict = dict()
             hostname = each[:each.rindex('-')]
-            unixtime = each[each.rindex('-')+1:]
+            unixtime = int(each[each.rindex('-') + 1:each.rindex(',')])
+            ntime = datetime.fromtimestamp(unixtime)
+
+            # Serialize datetime here!
+            ntime = ntime.strftime('%Y-%m-%d %H:%M:%S')
+
+            outdict.update({'hostname': hostname, 'unixtime': unixtime, 'normal time': ntime})
             with codecs.open(each, mode='r', encoding='utf-8-sig') as f:
                 file = f.read()
                 file = file.replace('\r', '').replace('\n', '')
                 data = json.loads(file)
+                outdict.update({'data': data})
+                outlist.append(outdict)
 
-                pass
-
-    def makeCollection(self):
-        pass
-
-
+        return outlist
 
 
 
 
 if __name__ == '__main__':
-    A = DataSoup(DIR)
+    fileslist = GetDataFiles.getFromDirectory(DIR)
+    A = DataSoup.parseDataFromFile(fileslist)
 
+    with open('data-out.json', mode='w', encoding='utf-8') as f:
+        json.dump(A, f, ensure_ascii=False, indent=4)
+
+    print('vse')
